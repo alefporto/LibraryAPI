35014e903aed4bfd719a08265e021c6ef1532473
Alef Porto <falejoaoalef@gmail.com>
2024-06-26 12:02:55 -0300

    Fixes validations in publisher and author models

--- src/models/Author.js
+++ src/models/Author.js
@@ -4,10 +4,12 @@ const authorSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
     name: {
         type: String,
+        cast: "O campo nome deve receber uma string válida",
         required: [true, "O campo nome é obrigatório"]
     },
     nationality: {
         type: String,
+        cast: "O campo nacionalidade deve receber uma string válida",
         required: [true, "O campo nacionalidade é obrigatório"]
     }
 }, { versionKey: false });
--- src/models/Publisher.js
+++ src/models/Publisher.js
@@ -4,10 +4,12 @@ const publisherSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
     name: {
         type: String,
+        cast: "O campo nome deve receber uma string válida",
         required: [true, "O campo nome é obrigatório"]
     },
     country: {
         type: String,
+        cast: "O campo país deve receber uma string válida",
         required: [true, "O campo país é obrigatório"]
     }
 }, { versionKey: false });
